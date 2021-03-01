5333690047fb3feedf3321bfe72cf3686ff86e10
Merkie <merkie@post.com>
2021-02-28 18:06:50 -0600

    test footage & cleaning up code

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import cv2
 import numpy
 
+
 def analyzeData(videofile):
-	
-	points = 0
+    vidcap = cv2.VideoCapture(videofile)
+    fps = int(vidcap.get(cv2.CAP_PROP_FPS))
 
-	vidcap = cv2.VideoCapture(videofile)
-	success, image = vidcap.read()
-	lastColorAverage = 0
-	while success:
-		#Write the image
-		cv2.imwrite("Step_One.jpg", image)
+    lastColorAverage = 0
+    points = 0
+    frameCount = 0
+    secondCounter = 0
 
-		#Get the average color
-		avg_color_per_row = numpy.average(image, axis=0)
-		avg_color = numpy.average(avg_color_per_row, axis=0)
-		x, y, z = avg_color
-		avgAvg = (x+y+z)/3
+    success, image = vidcap.read()
 
-		if lastColorAverage-avgAvg > 50 or lastColorAverage-avgAvg < -50:
-			points += 1
+    while success:
+        frameCount += 1
 
-		print(lastColorAverage-avgAvg)
+        # Get the average color
+        r, g, b = numpy.average(numpy.average(image, axis=0), axis=0)
+        avgAvg = (r + g + b) / 3
 
-		lastColorAverage = avgAvg
+        # If the difference between the two frames is greator or less than a certain threshold
+        if lastColorAverage - avgAvg > 50 or lastColorAverage - avgAvg < -50:
+            points += 1
 
-		success, image = vidcap.read()
-	
-	if(points > 10):
-			print(f"Video is seizure inducing. Total of {str(points)} seizure points.")
-	else:
-		print(f"video is not harmful with {str(points)} points.")
+        # Debug
+        if frameCount == fps:
+            if points > 0:
+                print(f"{points} points in a second {secondCounter}")
+            frameCount = 0
+            secondCounter += 1
+            points = 0
 
-analyzeData("minecraft.mp4")
+        # Reset the lastColorAverage
+        lastColorAverage = avgAvg
+
+        success, image = vidcap.read()
 
 
+analyzeData("tests/tests.mp4")
